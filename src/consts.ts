35cb72bf0fcd93ded8089ed5624403f4ab927129
steveluu <steveluu1123@gmail.com>
2025-04-11 17:19:27 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,9 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "Steve Luu",
   EMAIL: "steve@withpursuits.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 2,
   NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 0,
+  NUM_PROJECTS_ON_HOMEPAGE: 1,
 };
 
 export const HOME: Metadata = {
