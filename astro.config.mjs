7db9fa4ef3e8ab1a1cd1b2f0d46ce7263ebb7fd9
steveluu <steveluu1123@gmail.com>
2025-04-11 11:34:08 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://steveluu.com',
+  integrations: [tailwind() ],
 });
